1218f3d280edbf1dfc562f7a7ba2251a91b6e08f
Roman Martinez <roman.mx3@gmail.com>
2020-07-11 19:46:37 -0500

    Updated packages and recompiled css

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,8 @@
 let mix = require('laravel-mix');
 // mix.js(['_source/js/main.js'], 'assets/js/main.js');
 // mix.extract(['jquery']);
-mix.sass('_source/scss/main.scss', 'assets/css/', {outputStyle: 'expanded'});
-mix.sass('_source/scss/print.scss', 'assets/css/', {outputStyle: 'expanded'});
+mix.sass('_source/scss/main.scss', 'assets/css/', {sassOptions: {outputStyle: 'expanded'}});
+mix.sass('_source/scss/print.scss', 'assets/css/', {sassOptions: {outputStyle: 'expanded'}});
 mix.options({
 	autoprefixer: { options: { browsers: ['last 2 versions'] } },
 	processCssUrls: false,
